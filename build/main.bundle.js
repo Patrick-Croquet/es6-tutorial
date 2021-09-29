655a80d9506dd46f113c8c8b3c1aa405a5063e0e
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2021-09-29 10:56:53 +0200

    Initial commit

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -2,69 +2,57 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./js/mortgage.js":
-/*!************************!*\
-  !*** ./js/mortgage.js ***!
-  \************************/
+/***/ "./js/mortgage2.js":
+/*!*************************!*\
+  !*** ./js/mortgage2.js ***!
+  \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "calculateMonthlyPayment": () => (/* binding */ calculateMonthlyPayment),
-/* harmony export */   "calculateAmortization": () => (/* binding */ calculateAmortization)
+/* harmony export */   "default": () => (/* binding */ Mortgage)
 /* harmony export */ });
-let calculateMonthlyPayment = (principal, years, rate) => {
-  let monthlyRate = 0;
-
-  if (rate) {
-    monthlyRate = rate / 100 / 12;
+class Mortgage {
+  constructor(principal, years, rate) {
+    this.principal = principal;
+    this.years = years;
+    this.rate = rate;
   }
 
-  let monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-  return {
-    principal,
-    years,
-    rate,
-    monthlyPayment,
-    monthlyRate
-  };
-};
-let calculateAmortization = (principal, years, rate) => {
-  let {
-    monthlyRate,
-    monthlyPayment
-  } = calculateMonthlyPayment(principal, years, rate);
-  let balance = principal;
-  let amortization = [];
+  get monthlyPayment() {
+    let monthlyRate = this.rate / 100 / 12;
+    return this.principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), this.years * 12));
+  }
 
-  for (let y = 0; y < years; y++) {
-    let interestY = 0; //Interest payment for year y
+  get amortization() {
+    let monthlyPayment = this.monthlyPayment;
+    let monthlyRate = this.rate / 100 / 12;
+    let balance = this.principal;
+    let amortization = [];
 
-    let principalY = 0; //Principal payment for year y
+    for (let y = 0; y < this.years; y++) {
+      let interestY = 0;
+      let principalY = 0;
 
-    for (let m = 0; m < 12; m++) {
-      let interestM = balance * monthlyRate; //Interest payment for month m
+      for (let m = 0; m < 12; m++) {
+        let interestM = balance * monthlyRate;
+        let principalM = monthlyPayment - interestM;
+        interestY = interestY + interestM;
+        principalY = principalY + principalM;
+        balance = balance - principalM;
+      }
 
-      let principalM = monthlyPayment - interestM; //Principal payment for month m
-
-      interestY = interestY + interestM;
-      principalY = principalY + principalM;
-      balance = balance - principalM;
+      amortization.push({
+        principalY,
+        interestY,
+        balance
+      });
     }
 
-    amortization.push({
-      principalY,
-      interestY,
-      balance
-    });
+    return amortization;
   }
 
-  return {
-    monthlyPayment,
-    monthlyRate,
-    amortization
-  };
-};
+}
 
 /***/ })
 
@@ -131,20 +119,35 @@ var __webpack_exports__ = {};
   !*** ./js/main.js ***!
   \********************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _mortgage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./mortgage */ "./js/mortgage.js");
+/* harmony import */ var _mortgage2__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./mortgage2 */ "./js/mortgage2.js");
 
 document.getElementById('calcBtn').addEventListener('click', () => {
   let principal = document.getElementById("principal").value;
   let years = document.getElementById("years").value;
   let rate = document.getElementById("rate").value;
-  let {
-    monthlyPayment,
-    monthlyRate,
-    amortization
-  } = _mortgage__WEBPACK_IMPORTED_MODULE_0__.calculateAmortization(principal, years, rate);
-  document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-  document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-  amortization.forEach(month => console.log(month));
+  let mortgage = new _mortgage2__WEBPACK_IMPORTED_MODULE_0__["default"](principal, years, rate);
+  document.getElementById("monthlyPayment").innerHTML = mortgage.monthlyPayment.toFixed(2);
+  document.getElementById("monthlyRate").innerHTML = (rate / 12).toFixed(2);
+  let html = "";
+  mortgage.amortization.forEach((year, index) => html += `
+        <tr>
+            <td>${index + 1}</td>
+            <td class="currency">${Math.round(year.principalY)}</td>
+            <td class="stretch">
+                <div class="flex">
+                    <div class="bar principal"
+                         style="flex:${year.principalY};-webkit-flex:${year.principalY}">
+                    </div>
+                    <div class="bar interest"
+                         style="flex:${year.interestY};-webkit-flex:${year.interestY}">
+                    </div>
+                </div>
+            </td>
+            <td class="currency left">${Math.round(year.interestY)}</td>
+            <td class="currency">${Math.round(year.balance)}</td>
+        </tr>
+    `);
+  document.getElementById("amortization").innerHTML = html;
 });
 })();
 
